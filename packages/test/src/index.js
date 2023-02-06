907b455fafb96d293c1f8fa3779b639080935a4e
frodggy <frodggyxd@gmail.com>
2023-02-06 01:22:20 +0000

    init commit

---
+++ packages/test/src/index.js
@@ -0,0 +1,13 @@
+import Actinium from '@actiniumjs/core'
+
+
+function App() {
+    return (
+        <>
+            <p>Press me</p>
+        </>
+        
+    )
+}
+
+document.getElementById("app").appendChild(<App />)
